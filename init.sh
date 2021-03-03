063d358548b1c85e137efdf89e0b3511e66722c8
Olof Bjarnason <olof.bjarnason@gmail.com>
2021-03-03 11:41:59 +0100

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,4 @@
+# this script downloads catch2 'amalgamated' version; for proper use see their documentation on integrating with CMake/larger project!
+
+curl https://raw.githubusercontent.com/catchorg/Catch2/devel/extras/catch_amalgamated.cpp >catch_amalgamated.cpp
+curl https://raw.githubusercontent.com/catchorg/Catch2/devel/extras/catch_amalgamated.hpp >catch_amalgamated.hpp
