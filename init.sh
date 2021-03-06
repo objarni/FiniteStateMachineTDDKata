427c105ffe7ce64a5dff18c3ac033e7436ca1466
Olof Bjarnason <olof.bjarnason@gmail.com>
2021-03-06 07:23:43 +0100

    remove unneded setup script

--- init.sh
+++
@@ -1,4 +0,0 @@
-# this script downloads catch2 'amalgamated' version; for proper use see their documentation on integrating with CMake/larger project!
-
-curl https://raw.githubusercontent.com/catchorg/Catch2/devel/extras/catch_amalgamated.cpp >catch_amalgamated.cpp
-curl https://raw.githubusercontent.com/catchorg/Catch2/devel/extras/catch_amalgamated.hpp >catch_amalgamated.hpp
