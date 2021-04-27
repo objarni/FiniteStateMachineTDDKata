ad75e0eae3ce4b62ab5e56ddce3fab4d583f9d04
Olof Bjarnason <olof.bjarnason@gmail.com>
2021-04-27 08:03:14 +0200

    Update fsm.c

--- prod/fsm.c
+++ prod/fsm.c
@@ -6,17 +6,14 @@ void initButtonFSM(ButtonFsm *fsm) {
 }
 
 void buttonSignalHandler(ButtonFsm *fsm, Signal signal) {
-    unsigned char *port1 = getPortAddress(1);
     switch (fsm->state) {
         case waitForPress:
             if (signal == USER_PRESS) {
-                *port1 |= 1 << 7;
                 fsm->state = waitForElevator;
             }
             break;
         case waitForElevator:
             if (signal == DOORS_OPENING) {
-                *port1 ^= 1 << 7; // clear bit 7
                 fsm->state = waitForPress;
             }
             break;
