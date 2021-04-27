8a7e14fac320a6532241835e5220ca9a7a13806f
Olof Bjarnason <olof.bjarnason@gmail.com>
2021-04-27 08:02:37 +0200

    Update port.c

--- prod/port.c
+++ prod/port.c
@@ -1,10 +1,9 @@
 
-unsigned char* port0Address= 0xff3927;
 
-unsigned char* getPortAddress(int portNumber) {
-    return port0Address + portNumber;
-}
+// GLOBAL VARIABLES POINTING TO PORT MEMORY ADDRESSES
+// Override these in tests to test Button FSM
 
-void setPortAddress0(unsigned char* address) {
-    port0Address = address;
-}
+unsigned char* port0Address = (unsigned char *)0xff3927;
+unsigned char* port1Address = (unsigned char *)0xff3928;
+unsigned char* port2Address = (unsigned char *)0xff3929;
+unsigned char* port3Address = (unsigned char *)0xff392A;
